50f8d45c6e03e85ffb162548becab76efbf82d59
Vindaar <basti90@gmail.com>
2022-06-07 13:11:37 +0200

    use unicode by default in pretty printing

--- src/mpfit.nim
+++ src/mpfit.nim
@@ -83,7 +83,7 @@ func formatValue*(f: float, precision: int): string =
   result.trimZeros()
 
 proc pretty*(x: openArray[float], res: mp_result, xact: openArray[float] = @[],
-             unicode = false, precision = -1, prefix = "  "): string =
+             unicode = true, precision = -1, prefix = "  "): string =
   ## A convenience proc to echo the fit parameters and their errors as well
   ## as the properties of the fit, e.g. chi^2 etc.
   ##
